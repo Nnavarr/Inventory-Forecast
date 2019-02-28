8a706ac3e27874a12d512795ace3c57fc2722d72
Nnavarr <noe.navarro1019@gmail.com>
2019-02-28 10:32:53 -0700

    Upload App file to git

---
+++ Inventory_Forecast_Tool/app.R
@@ -0,0 +1,89 @@
+
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(plotly)
+library(dplyr)
+library(ggplot2)
+library(markdown)
+library(rjson)
+library(RCurl)
+
+# -----------------
+# Shiny app UI ----
+# -----------------
+
+# App Architecture ----
+header <- dashboardHeader(
+  title = "Forecast Tool"
+)
+
+# Side Bar ----
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    
+    # Create Menu ----
+    menuItem(text = 'Forecast Summary',
+             tabName = 'summary'),
+    
+    sidebarLayout(
+      sidebarPanel(
+        fileInput('data', 'Choose CSV file to upload',
+                  accept = '.csv'
+        )
+      ),
+      mainPanel(
+        tableOutput('contents')
+      
+    )
+  )
+)
+)
+
+
+#TODO Enter button to upload CSV Data 
+# This imported dataframe will be referenced directly in the line below
+
+
+
+# Control Dropdowns ----
+body <- dashboardBody(
+fixedRow(
+ column(1, selectInput(inputId = "Part", label = "Part Number", choices = 'Test', selected = "13002"))
+ )
+)
+
+# Define UI for application (Dashboard) ----
+ui <- dashboardPage(header = header,
+                    sidebar = sidebar,
+                    body = body)
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# -------------
+# Server.R ----
+# -------------
+
+server <- function(input, output) {
+  output$contents <- renderTable({
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, it will be a data frame with 'name',
+    # 'size', 'type', and 'datapath' columns. The 'datapath'
+    # column will contain the local filenames where the data can
+    # be found.
+    
+    inFile <- input$data
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    read.csv(inFile$datapath, header = input$header,
+             sep = input$sep, quote = input$quote)
+  })
+}
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
